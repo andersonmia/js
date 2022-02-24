4d84f76c21c2603de17c1eb3d0d024f2c0f3bd76
MAJURA Gaudance <96429597+andersonmia@users.noreply.github.com>
2022-02-24 01:38:04 -0800

    Update alphabetical.js
    js to print the letters forming a word alphabetically

--- alphabetical.js
+++ alphabetical.js
@@ -1,14 +1,17 @@
-function alphabeticalSort(word) {
-    let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    let arrangedWord = ''
-    for (i = 0; i < letters.length; i++) {
-        for (j = 0; j < word.length; j++) {
-            if (letters[i] == word[j]) {
-                arrangedWord += word[j]
-            }
+function alphOrder(word){
+    let alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    let arranged = '';
+    
+    for (let i = 0; i < alphabets.length; i++) {
+        for (let j = 0; j < word.length; j++) {
+          if (alphabets[i] == word[j]) {
+              arranged += word[j];
+              
+          }  
         }
+       
     }
-    console.log(arrangedWord)
-}
-alphabeticalSort('woman');
-alphabeticalSort('Relatively');
+    console.log(arranged);
+    }
+    alphOrder("teetotaller");
+    alphOrder("anderson");
