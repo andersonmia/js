d4561f3bd4259c59e3094cbe3a324d8566b5cb91
MAJURA Gaudance <96429597+andersonmia@users.noreply.github.com>
2022-03-06 11:53:06 -0800

    oop

---
+++ personClass.js
@@ -0,0 +1,10 @@
+//question 5
+function Person(dob,gender,interests){
+    this.dob = dob;
+    this.gender = gender;
+    this.interests = interests;
+}
+ const mia = new Person({day:2, month:3, year:2005},"female",["football","basketball"]);
+ console.log(mia.dob);
+
+ //question 6 
